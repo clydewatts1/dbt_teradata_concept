e904e7a52ea74b7bbea9bef99087fafe2c19efda
Clyde Watts <Clyde.Watts267@gmail.com>
2025-05-16 14:15:09 +0000

    dob

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,4 +1,3 @@
-
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
@@ -13,7 +12,7 @@ with source_data as (
 
     select distinct 1 as id from dbc.dbcinfo
     union all
-    select distinct  null as id  from dbc.dbcinfo
+    select distinct null as id from dbc.dbcinfo
 
 )
 
